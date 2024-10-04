2d7cca65ba5d9e9979dbfda11d2162b5765b067e
gilangaryo <gilang91668@gmail.com>
2024-10-04 17:06:02 +0700

    update index

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const { notFound } = require('./src/middleware/errorHandlers.js');
+const { errorMiddleware } = require('./src/middleware/error-middleware.js');
+const cookieParser = require('cookie-parser');
 
-const app = express()
-const PORT = 8000
+// Import routes
+const userRoutes = require('./src/useCase/user/index.js');
+const authRoutes = require('./src/useCase/auth/index.js');
+const poleRoutes = require('./src/useCase/pole/index.js');
+const vendorRoutes = require('./src/useCase/vendor/index.js');
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5002;
+const app = express();
+app.use(express.json());
+
+//PARSE APPLICATION JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'))
+app.use(morgan(':date[web]'))
+app.use(cors());
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Welcome to WIKA API!');
+});
 
-app.get('/about', (req, res) => {
-    res.send('About route 🎉 ')
-})
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use('/api/pole', poleRoutes);
+app.use('/api/vendor', vendorRoutes);
+
+// Middleware for handling "Not Found" errors (404)
+app.use(notFound);
+app.use(errorMiddleware);
+
+// Middleware for handling other errors (errorStatus)
+// app.use(errorStatus);
 
 app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}`);
-})
+    console.log('Server running port: ', PORT);
+});
